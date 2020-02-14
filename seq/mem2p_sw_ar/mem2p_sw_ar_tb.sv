149e0684833b0e4ae6dc6d97212b701810104e25
John Nestor <nestorj@lafayette.edu>
2020-02-13 22:27:08 -0500

    huh?

---
+++ seq/mem2p_sw_ar/mem2p_sw_ar_tb.sv
@@ -0,0 +1,58 @@
+//-----------------------------------------------------------------------------
+// Title         : mem2p_sw_ar_tb Testbench for 2-port memory
+// Project       : RTL Hardware Design and Verification using SystemVerilog
+//-----------------------------------------------------------------------------
+// File          : mem2p_sw_ar.sv
+// Author        : John Nestor  <nestorj@nestorj-mbpro-15.home>
+// Created       : 13.02.2020
+// Last modified : 13.02.2020
+//-----------------------------------------------------------------------------
+// Description :
+// Simple stimulus-only testbench for a two-port memory with a single
+// clock, a synchronous write port and a synchronous read port.
+//------------------------------------------------------------------------------
+// Modification history :
+// 13.02.2020 : created
+//-----------------------------------------------------------------------------
+
+module mem2p_sw_ar_tb;
+  parameter W=8, D=16, DW=$clog2(D);
+  logic clk, we1;
+  logic [DW-1:0] addr1;
+  logic [W-1:0] din1;
+  logic [DW-1:0] addr2;
+  logic [W-1:0] dout2;
+
+
+  mem2p_sw_sr #(.W(W), .D(D)) DUV (.clk, .we1, .din1, .addr1,
+                                    .addr2, .dout2);
+
+  always begin
+    clk = 0; #10;
+    clk = 1; #10;
+  end
+
+  initial begin
+    $monitor("time=%t we1=%b addr1=%h din1=%h addr2=%h dout2=%h",
+             $time, we1, addr1, din1, addr2, dout2);
+             
+    we1 = 0;
+    addr1 = 4'd0;
+    din1 = 8'd0;
+    addr2 = 4'd0;
+    @(posedge clk) #1;
+    we1 = 1;
+    @(posedge clk) #1;
+    addr1 = 4'd1;
+    din1 = 8'd1;
+    @(posedge clk) #1;
+    addr1 = 4'd2;
+    din1 = 8'hF2;
+    addr2 = 4'd1;
+    @(posedge clk) #1;
+    we1 = 0;
+    addr2 = 4'd2;
+    @(posedge clk) #1;
+    $stop;
+  end
+endmodule
---
+++ seq/mem2p_sw_ar/mem2p_sw_sr.sv
@@ -0,0 +1,37 @@
+//-----------------------------------------------------------------------------
+// Title         : mem2p_sw_ar 2-port memory
+// Project       : RTL Hardware Design and Verification using SystemVerilog
+//-----------------------------------------------------------------------------
+// File          : mem2p_sw_ar.sv
+// Author        : John Nestor  <nestorj@nestorj-mbpro-15.home>
+// Created       : 13.02.2020
+// Last modified : 13.02.2020
+//-----------------------------------------------------------------------------
+// Description :
+// Two-port memory with a single clock, a synchronous write port
+// and a synchronous read port.  This will synthesize as blocki
+// RAM in a Xilinx FPGA
+//------------------------------------------------------------------------------
+// Modification history :
+// 13.02.2020 : created
+//-----------------------------------------------------------------------------
+
+  module mem2p_sw_sr #(parameter W=8, D=128, localparam DW=$clog2(D))
+    (
+    input logic clk,
+    input logic we1,
+    input logic [DW-1:0] addr1,
+    input logic [W-1:0]  din1,
+    input logic [DW-1:0] addr2,
+    output logic [W-1:0] dout2
+    );
+
+  logic [W-1:0] ram_array [D-1:0];
+
+  always_ff @(posedge clk)
+    if (we1) ram_array[addr1] = din1;
+
+  always_ff @(posedge clk)
+    dout2 = ram_array[addr2];
+
+endmodule
