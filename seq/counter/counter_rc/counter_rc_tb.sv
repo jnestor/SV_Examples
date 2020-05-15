803acfb079af4726b01005776519fe575db1b266
nestorj <nestorj@lafayette.edu>
2020-05-15 11:38:40 -0400

    oops one more change

--- seq/counter/counter_rc/counter_rc_tb.sv
+++ seq/counter/counter_rc/counter_rc_tb.sv
@@ -3,10 +3,12 @@
 module #(parameter CLKPD=100) counter_rc_tb
         (input logic clk,
          output logic rst, enb,
-         input logic [3:0] q,    // not used in stimulus-only
-         input logic       cy);  // not used in stimulus-only
+         input logic [3:0] q,
+         input logic       cy);
 
   initial begin
+    $monitor("time=%t clk=%b rst=%b enb=%b q=%h cy=%b",
+             $time, clk, rst, enb, q, cy);
     rst = 1;  // initial reset
     enb = 0;
     #(CLKPD+1);  // resume 1 time unit after clock edge
