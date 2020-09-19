5869e80c269774e1c50af65bad45cf4ae2914267
nestorj <nestorj@lafayette.edu>
2020-09-19 11:16:25 -0400

    added combinational double dabble example

---
+++ comb/dbl_dabble/dbl_dabble.sv
@@ -0,0 +1,19 @@
+module dbl_dabble(input logic [7:0] b,
+                  output logic [3:0] hundreds, tens, ones);
+
+logic [7:0] bs;
+
+always_comb
+    begin
+        bs = b;
+        {hundreds, tens, ones} = 12'h0;
+        for (int i=0; i<8; i++)
+            begin
+                if (ones >= 4'd5) ones = ones + 3;
+                if (tens >= 4'd5) tens = ones + 3;
+                if (hundreds >= 4'd5) hundreds = hundreds + 3;
+                {hundreds, tens, ones, bs} = {hundreds, tens, ones, bs} << 1;
+            end
+    end
+
+endmodule: dbl_dabble
---
+++ comb/dbl_dabble/dbl_dabble_tb.sv
@@ -0,0 +1,24 @@
+module dbl_dabble_tb;
+    logic  [7:0] b;
+    logic [3:0] hundreds, tens, ones;
+    logic [3:0] hundreds_exp, tens_exp, ones_exp;
+    dbl_dabble DUV (.b, .hundreds, .tens, .ones);
+
+    initial begin
+        $timeformat(-9,2," ns");
+        for (int i=0; i<256; i++) begin
+            b = i;
+            ones_exp = b % 10;
+            tens_exp = (b / 10) % 10;
+            hundreds_exp = b / 100;
+            #10;
+            if (hundreds_exp != hundreds)
+                $display("time=%t b=%d expected hundreds: %d actual %d",$time,b,hundreds_exp, hundreds);
+            if (tens_exp != tens)
+                $display("time=%t b=%d expected tens: %d actual %d",$time,b,tens_exp, tens);
+            if (ones_exp != ones)
+                $display("time=%t b=%d expected ones: %d actual %d",$time,b,ones_exp, ones);
+        end
+        $stop;
+    end
+endmodule
