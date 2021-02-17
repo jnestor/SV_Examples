0bf5526539d77c8d38b8598ca4da59940957f394
John Nestor <nestorj@lafayette.edu>
2021-02-17 14:59:02 -0500

    updated double dabble

--- comb/dbl_dabble_f/dbl_dabble_ext_tb.sv
+++ comb/dbl_dabble_f/dbl_dabble_ext_tb.sv
@@ -1,34 +1,37 @@
 //-----------------------------------------------------------------------------
-// Module Name   : dbl_dabble_ext_tb - testbench for binary-bcd converter
+// Module Name   : dbl_dabble_ext_tb - testbench for ext. binar-bcd conv.
 // Project       : RTL Hardware Design and Verification using SystemVerilog
 //-----------------------------------------------------------------------------
 // Author        : John Nestor  <nestorj@lafayette.edu>
 // Created       : Feb 2021
 //-----------------------------------------------------------------------------
-// Description   : Self-checking testbench for the double-dabble
+// Description   : Self-checking testbench for the extended double-dabble
 // binary-bcd converter.  Exhaustively tests all possble values and reports
 // errors
 //-----------------------------------------------------------------------------
 
 module dbl_dabble_tb;
-    logic  [7:0] b;
-    logic [3:0] hundreds, tens, ones;
-    logic [3:0] hundreds_exp, tens_exp, ones_exp;
-    dbl_dabble DUV (.b, .hundreds, .tens, .ones);
+    logic  [9:0] b;
+    logic [3:0] thousands, hundreds, tens, ones;
+    logic [3:0] thousands_exp, hundreds_exp, tens_exp, ones_exp;
+    dbl_dabble_ext DUV (.b, .thousands, .hundreds, .tens, .ones);
 
     initial begin
         $timeformat(-9,2," ns");
-        b = 255;
+        b = 1023;
         #10;
         b = 123;
         #10;
         $stop;
-        for (int i=0; i<256; i++) begin
+        for (int i=0; i<1024; i++) begin
             b = i;
             ones_exp = b % 10;
             tens_exp = (b / 10) % 10;
-            hundreds_exp = b / 100;
+            hundreds_exp = b / 100 % 10;
+            thousands_exp = b / 1000;
             #10;
+            if (thousands_exp != thousands)
+                $display("time=%t b=%d expected thousands: %d actual %d",$time,b,thousands_exp, thousands);
             if (hundreds_exp != hundreds)
                 $display("time=%t b=%d expected hundreds: %d actual %d",$time,b,hundreds_exp, hundreds);
             if (tens_exp != tens)
