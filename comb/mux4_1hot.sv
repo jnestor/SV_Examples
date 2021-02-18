d37165408d18ed8593e11fbaf82a5a027dc44df5
nestorj <nestorj@lafayette.edu>
2021-02-18 15:55:06 -0500

    edited mux4_1hot

--- comb/mux4_1hot.sv
+++ comb/mux4_1hot.sv
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// Module Name   : mux4
+// Module Name   : mux4_1hot
 // Project       : RTL Hardware Design and Verification using SystemVerilog
 //-----------------------------------------------------------------------------
 // Author        : John Nestor  <nestorj@lafayette.edu>
@@ -8,18 +8,18 @@
 // Description   : 4-1 mux parameterized by bitwidthw W
 //-----------------------------------------------------------------------------
 
-module mux4 #(parameter W=32)
+module mux4_1hot #(parameter W=32)
              (input logic [W-1:0]  d0, d1, d2, d3,
-              input logic [1:0]    sel,
+              input logic [3:0]    sel,
               output logic [W-1:0] y );
 
    always_comb
-     case (sel)
-       2'd0 : y = d0;
-       2'd1 : y = d1;
-       2'd2 : y = d2;
-       2'd3 : y = d3;
+     unique case (sel)
+       4'd1000 : y = d0;
+       4'd0100 : y = d1;
+       4'd0010 : y = d2;
+       2'd0001 : y = d3;
        default : y = 0;
      endcase
 
-endmodule: mux4
+endmodule: mux4_1hot
