305be4459eef83b503743e42754837fe0208980a
John Nestor <nestorj@lafayette.edu>
2022-02-03 12:29:01 -0500

    further extended dbl_dabble_f to handle 13 bit inputs / 4 digit output

--- comb/dbl_dabble_f/dbl_dabble_ext.sv
+++ comb/dbl_dabble_f/dbl_dabble_ext.sv
@@ -3,24 +3,26 @@
 // Project       : RTL Hardware Design and Verification using SystemVerilog
 //-----------------------------------------------------------------------------
 // Author        : John Nestor  <nestorj@lafayette.edu>
-// Created       : Feb 2021
+// Created       : Feb 2021, updated Feb 2022
 //-----------------------------------------------------------------------------
 // Description   : Functional description of an extended binary-bcd
 //  converter using the double-dabble algorithm implemented in combinational
-// logic.  This version converts a 10-bit input to four bcd digits output
+// logic.  This version converts a 13-bit input to four bcd digits output
+// over a range between 0-8191.  Note that the $display calls are
+// ignored during synthesis but illustrate the operation of the algorithm
 //-----------------------------------------------------------------------------
 
-module dbl_dabble_ext(input logic [9:0] b,
+module dbl_dabble_ext(input logic [12:0] b,
                   output logic [3:0] thousands, hundreds, tens, ones);
 
-logic [9:0] bs;
+logic [12:0] bs;
 
 always_comb
     begin
         $display("dbl_dabble input: %d", b);
         bs = b;
         {thousands, hundreds, tens, ones} = 16'h0;
-        for (int i=1; i<=10; i++)
+        for (int i=1; i<=13; i++)
             begin
                 if ((i > 3) && (ones >= 4'd5)) begin
                     ones = ones + 3;
@@ -34,9 +36,11 @@ always_comb
                     hundreds = hundreds + 3;
                     $display("add3o %1d %4b %4b %4b %4b %8b", i, thousands, hundreds, tens, ones, bs);
                 end
-                // don't need to check thousands9 digit - it will never go above 3!
+                if ((i > 12) && (thousands >= 4'd5)) begin
+                    thousands = thousands + 3;
+                    $display("add3o %1d %4b %4b %4b %4b %8b", i, thousands, hundreds, tens, ones, bs);
+                end
                 {thousands, hundreds, tens, ones, bs} = {thousands, hundreds, tens, ones, bs} << 1;
-                $display("add3o %1d %4b %4b %4b %4b %8b", i, thousands, hundreds, tens, ones, bs);
             end
             $display("result    %d    %d   %d   %d", thousands, hundreds, tens, ones);
     end
--- comb/dbl_dabble_f/dbl_dabble_ext_tb.sv
+++ comb/dbl_dabble_f/dbl_dabble_ext_tb.sv
@@ -11,34 +11,46 @@
 //-----------------------------------------------------------------------------
 
 module dbl_dabble_tb;
-    logic  [9:0] b;
+    logic  [12:0] b;
     logic [3:0] thousands, hundreds, tens, ones;
     logic [3:0] thousands_exp, hundreds_exp, tens_exp, ones_exp;
     dbl_dabble_ext DUV (.b, .thousands, .hundreds, .tens, .ones);
+    int err_ct;
 
     initial begin
         $timeformat(-9,2," ns");
+        err_ct = 0;
         b = 1023;
         #10;
         b = 123;
         #10;
+        b = 1049;
+        #10;
+        b = 3000;
+        #10;
+        b = 4095;
+        #10;
+        b = 8000;
+        #10;
+        b = 8191;
+        #10;
         $stop;
-        for (int i=0; i<1024; i++) begin
+        for (int i=0; i<8191; i++) begin
             b = i;
             ones_exp = b % 10;
             tens_exp = (b / 10) % 10;
             hundreds_exp = b / 100 % 10;
             thousands_exp = b / 1000;
             #10;
-            if (thousands_exp != thousands)
-                $display("time=%t b=%d expected thousands: %d actual %d",$time,b,thousands_exp, thousands);
-            if (hundreds_exp != hundreds)
-                $display("time=%t b=%d expected hundreds: %d actual %d",$time,b,hundreds_exp, hundreds);
-            if (tens_exp != tens)
-                $display("time=%t b=%d expected tens: %d actual %d",$time,b,tens_exp, tens);
-            if (ones_exp != ones)
-                $display("time=%t b=%d expected ones: %d actual %d",$time,b,ones_exp, ones);
+            if ( (thousands_exp != thousands) || (hundreds_exp != hundreds)
+              || (tens_exp != tens) || (ones_exp != ones) ) begin
+                  $display("time=%t, expected %1d %1d %1d %1d actual %1d %1d %1d %1d",
+                            $time, thousands_exp, hundreds_exp, tens_exp, ones_exp,
+                            thousands, hundreds, tens, ones);
+                  err_ct++;
+              end
         end
+        $display("%d errors", err_ct );
         $stop;
     end
 endmodule
