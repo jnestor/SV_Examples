e3a5c25207bae6dc8707fae566e302d91e3d728b
nestorj <nestorj@lafayette.edu>
2023-04-04 14:21:54 -0400

    fixed typos in dregre.sv

--- comb/mux8.sv
+++ comb/mux8.sv
@@ -8,10 +8,12 @@
 // Description   : 8-1 mux parameterized by bitwidthw W
 //-----------------------------------------------------------------------------
 
-module mux8 #(parameter W=1)
-	         (input logic [W-1:0] d0, d1, d2, d3, d4, d5, d6, d7,
-	          input logic [2:0] sel,
-	          output logic [W-1:0] y);
+module mux8 #(parameter W=4) (
+    input logic [W-1:0] d0, d1, d2, d3, d4, d5, d6, d7,
+	  input logic [2:0] sel,
+	  output logic [W-1:0] y
+    );
+   
    always_comb
      case (sel)
        3'd0 : y = d0;
--- seq/dreg/dregre.sv
+++ seq/dreg/dregre.sv
@@ -8,13 +8,15 @@
 // Description   : d register with synchronous reset and enable
 //-----------------------------------------------------------------------------
 
-module dregre (#parameter W=4)
-              (input logic clk, rst, enb
-               input logic [W-1:0] 	d,
-               output logic [W-1:0] q);
+module dregre #(parameter W=4) (
+    input logic clk, rst, enb,
+    input logic [W-1:0] d,
+    output logic [W-1:0] q
+    );
 
     always_ff @(posedge clk) begin
         if (rst) q <= '0;
         else if (enb) q <= d;
+    end
 
 endmodule: dregre
